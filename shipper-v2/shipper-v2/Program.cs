8bf4e76e585efc482f58b5ef5a056f9308a7dea4
Brandon Berney <brandonberney@protonmail.com>
2020-09-04 10:08:50 -0400

    Add project files

---
+++ shipper-v2/shipper-v2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Grading ID: E3785
+ * Description: TBD
+ * Variable Dictionary:
+ */
+
+using System;
+
+namespace shipper_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
