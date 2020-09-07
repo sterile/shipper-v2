e2b51fe900ca239ba96536b0bbb508b4ebde3f38
Brandon Berney <brandonberney@protonmail.com>
2020-09-06 23:46:53 -0400

    Parcel: add the parcel class

--- shipper-v2/shipper-v2/Parcel.cs
+++ shipper-v2/shipper-v2/Parcel.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace shipper_v2
 {
-    class Parcel
+    abstract class Parcel
     {
+        private Address _origin, // Origin address
+            _destination;        // Destination address
+
+        public Parcel(Address origin, Address destination)
+        {
+            OriginAddress = origin;
+            DestinationAddress = destination;
+        }
+
+        public Address OriginAddress
+        {
+            get => _origin;
+            set => _origin = value ?? throw new ArgumentOutOfRangeException(nameof(OriginAddress), value, $"{nameof(OriginAddress)} must not be null.");
+        }
+
+        public Address DestinationAddress
+        {
+            get => _destination;
+            set => _destination = value ?? throw new ArgumentOutOfRangeException(nameof(DestinationAddress), value, $"{nameof(DestinationAddress)} must not be null.");
+        }
+
+        public abstract decimal CalcCost();
+
+        public override string ToString()
+        {
+            string NL = Environment.NewLine;
+            return $"ORIGIN: {NL}{OriginAddress}{NL}" +
+                $"{NL}DESTINATION:{NL}{DestinationAddress}{NL}" +
+                $"{NL}COST: {CalcCost():C}";
+        }
     }
 }
