d0469aa3fe0d2fc0e4ff20627956c381ea397bcf
Brandon Berney <brandonberney@protonmail.com>
2020-09-06 23:47:08 -0400

    Letter: add the letter class

--- shipper-v2/shipper-v2/Letter.cs
+++ shipper-v2/shipper-v2/Letter.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace shipper_v2
 {
-    class Letter
+    class Letter : Parcel
     {
+        private decimal cost; // Cost of shipment
+
+        public Letter(Address origin, Address destination, decimal cost) : base(origin, destination)
+        {
+            Cost = cost;
+        }
+
+        private decimal Cost
+        {
+            get => cost;
+            set
+            {
+                if (value <= decimal.Zero)
+                    throw new ArgumentOutOfRangeException(nameof(Cost), value,
+                        $"{nameof(Cost)} must be greater than {decimal.Zero:C}.");
+
+                cost = value;
+            }
+        }
+        public override decimal CalcCost() => Cost;
+
+        public override string ToString() => base.ToString() +
+            $"{Environment.NewLine}{Environment.NewLine}COST: {CalcCost():C}";
     }
 }
