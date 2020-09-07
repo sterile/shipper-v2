83fe63a4a7f0be80820f94ef2e860ed14b2ceca2
Brandon Berney <brandonberney@protonmail.com>
2020-09-06 23:50:00 -0400

    Program: add test data

--- shipper-v2/shipper-v2/Program.cs
+++ shipper-v2/shipper-v2/Program.cs
@@ -5,6 +5,7 @@
  */
 
 using System;
+using System.Collections.Generic;
 
 namespace shipper_v2
 {
@@ -12,7 +13,21 @@ namespace shipper_v2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Address home1 = new Address("Helen C. Bice", "1163 Thompson Drive", "El Sobrante", "CA", 94803);
+            Address home2 = new Address("Teresa T. Johnson", "3542 Farland Street", "Apt 101", "Westborough", "MA", 01581);
+            Address home3 = new Address("Troy H. Thomas", "1299 Saints Alley", "Plant City", "FL", 33564);
+            Address home4 = new Address("Susan K. McCrady", "3118 Chenoweth Drive", "Apt B4", "Clarksville", "TN", 37040);
+
+            var letters = new List<Parcel>();
+            letters.Add(new Letter(home1, home2, 0.43M));
+            letters.Add(new Letter(home2, home3, 0.39M));
+            letters.Add(new Letter(home4, home1, 0.47M));
+            letters.Add(new Letter(home2, home4, 1.23M));
+
+            foreach (Letter letter in letters)
+            {
+                Console.WriteLine(letter.ToString());
+            }
         }
     }
 }
